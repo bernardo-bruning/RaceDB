afde6805215ba11d6a87ebd5ed443e5757eb019e
Bernardo de Oliveira Bruning <bernardo.bruning@gmail.com>
2019-03-29 00:43:31 -0300

    🔨 improve store to generic types with impl trait Read, Write and Seek

--- src/storage.rs
+++ src/storage.rs
@@ -3,6 +3,7 @@ use std::io::Write;
 use std::io::Seek;
 use std::io::Cursor;
 use crate::pagination::Pages;
+use crate::pagination::Page;
 use crate::serialization::Serializable;
 use std::io::Error;
 
@@ -12,7 +13,19 @@ pub trait Storable: Sized+Serializable {
 
 pub trait Store: Read+Write+Seek {}
 
-impl Store for Cursor<Vec<u8>> {}
+impl <TStore: Read+Write+Seek> Store for TStore {}
+
+impl Storable for Page {
+  fn store(self, store: &mut Store) -> Result<Self, Error>
+  {
+    let source_serialized: Vec<u8> = self.serialize();
+    let result = store.write(&source_serialized);
+    match result {
+      Ok(_) => Ok(self),
+      Err(error) => Err(error)
+    }
+  }
+}
 
 impl Storable for Pages {
   fn store(self, store: &mut Store) -> Result<Self, Error>
