743331ef68d56cbc77c53437af8b6005657f61cc
Bernardo de Oliveira Bruning <bernardo.bruning@gmail.com>
2019-03-29 00:38:21 -0300

    🔨 create Store trait

--- src/storage.rs
+++ src/storage.rs
@@ -1,19 +1,23 @@
 use std::io::Read;
 use std::io::Write;
 use std::io::Seek;
+use std::io::Cursor;
 use crate::pagination::Pages;
 use crate::serialization::Serializable;
 use std::io::Error;
 
 pub trait Storable: Sized+Serializable {
-  fn store<Store: Read+Write+Seek>(self, store: &mut Store) -> Result<Self, Error>;
+  fn store(self, store: &mut Store) -> Result<Self, Error>;
 }
 
+pub trait Store: Read+Write+Seek {}
+
+impl Store for Cursor<Vec<u8>> {}
+
 impl Storable for Pages {
-  fn store<Store: Read+Write+Seek>(self, store: &mut Store) -> Result<Self, Error>
+  fn store(self, store: &mut Store) -> Result<Self, Error>
   {
     let source_serialized: Vec<u8> = self.serialize();
-
     let result = store.write(&source_serialized);
     match result {
       Ok(_) => Ok(self),
